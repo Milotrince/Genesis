4dc61b59c1d6d1d102d6c8991f0ef37edffaf86f
Trinity Chung <trinityjchung@gmail.com>
2025-08-07 16:07:28 -0700

    add imu sensor

---
+++ genesis/sensors/imu.py
@@ -0,0 +1,110 @@
+from typing import List, Tuple
+
+import numpy as np
+import taichi as ti
+import torch
+
+from genesis.options.sensors import SensorOptions
+from genesis.utils.geom import (
+    euler_to_quat,
+    inv_transform_by_trans_quat,
+    transform_quat_by_quat,
+)
+
+from .base_sensor import Sensor
+from .sensor_manager import register_sensor
+
+
+@ti.data_oriented
+class IMU(Sensor):
+    CACHE_DTYPE = torch.float32
+    CACHE_SHAPE = (2, 3)  # linear_acceleration, angular_velocity
+
+    def build(self):
+        super().build()
+        assert (
+            self._options.return_accelerometer or self._options.return_gyroscope
+        ), "At least one of return_accelerometer or return_gyroscope should be True."
+        self._solver = self._manager._sim.rigid_solver
+        assert self._options.link_idx >= 0 and self._options.link_idx < self._solver.n_links, "Invalid RigidLink index."
+        self._link_idx = self._options.link_idx
+
+        quat_offset = euler_to_quat(self._options.euler_offset)
+
+        if len(self._shared_metadata) == 0:
+            self._shared_metadata["links_idx"] = []
+            self._shared_metadata["offsets_pos"] = torch.tensor([], dtype=torch.float32)
+            self._shared_metadata["offsets_quat"] = torch.tensor([], dtype=torch.float32)
+
+        self._shared_metadata["links_idx"].append(self._link_idx)
+        self._shared_metadata["offsets_pos"] = torch.cat(
+            [self._shared_metadata["offsets_pos"], torch.tensor([self._options.pos_offset], dtype=torch.float32)]
+        )
+        self._shared_metadata["offsets_quat"] = torch.cat(
+            [self._shared_metadata["offsets_quat"], torch.tensor([quat_offset], dtype=torch.float32)]
+        )
+
+    def read(self, envs_idx: List[int] | None = None) -> Tuple[np.ndarray, np.ndarray]:
+        """Returns tuple(linear_acceleration, angular_velocity)."""
+        if not self._is_cache_updated():
+            # updated shared cache (all IMUs in all envs)
+            gravity = self._solver.get_gravity()
+            quats = self._solver.get_links_quat(links_idx=self._shared_metadata["links_idx"])
+            acc = self._solver.get_links_acc(links_idx=self._shared_metadata["links_idx"])
+            ang = self._solver.get_links_ang(links_idx=self._shared_metadata["links_idx"])
+            if self._solver.n_envs == 0:
+                gravity = gravity.unsqueeze(0)
+                quats = quats.unsqueeze(0)
+                acc = acc.unsqueeze(0)
+                ang = ang.unsqueeze(0)
+
+            offset_quats = transform_quat_by_quat(
+                quats, self._shared_metadata["offsets_quat"].unsqueeze(0).repeat(quats.shape[0], 1, 1)
+            )
+            # acc/ang shape: (B, n_links, 3)
+            local_acc = inv_transform_by_trans_quat(acc, self._shared_metadata["offsets_pos"], offset_quats)
+            local_ang = inv_transform_by_trans_quat(ang, self._shared_metadata["offsets_pos"], offset_quats)
+
+            local_acc = local_acc - gravity.unsqueeze(1).repeat(1, local_acc.shape[1], 1)
+
+            # cache shape: (B, n_links, 2, 3)
+            self._cache.copy_(torch.cat([local_acc.unsqueeze(2), local_ang.unsqueeze(2)], dim=2))
+            self._set_cache_updated()
+
+        if envs_idx is None:
+            envs_idx = 0 if self._solver.n_envs == 0 else np.arange(self._cache.shape[0])
+
+        ret = []
+        if self._options.return_accelerometer:
+            ret.append(self._cache[envs_idx, self._cache_idx, 0, :].squeeze())
+        if self._options.return_gyroscope:
+            ret.append(self._cache[envs_idx, self._cache_idx, 1, :].squeeze())
+        return tuple(ret)
+
+
+@register_sensor(IMU)
+class IMUOptions(SensorOptions):
+    """
+    IMU sensor returns the linear acceleration (accelerometer) and angular velocity (gyroscope)
+    of the associated entity link.
+
+    Parameters
+    ----------
+    link_idx : int
+        The global index of the RigidLink to which this IMU sensor is attached.
+    pos_offset : tuple[float, float, float]
+        The offset of the IMU sensor from the RigidLink.
+    euler_offset : tuple[float, float, float]
+        The offset of the IMU sensor from the RigidLink in euler angles.
+    return_accelerometer : bool
+        Whether to return the linear acceleration (accelerometer).
+    return_gyroscope : bool
+        Whether to return the angular velocity (gyroscope).
+    """
+
+    link_idx: int
+    pos_offset: tuple[float, float, float] = (0.0, 0.0, 0.0)
+    euler_offset: tuple[float, float, float] = (0.0, 0.0, 0.0)
+
+    return_accelerometer: bool = True
+    return_gyroscope: bool = True
---
+++ tests/test_sensors.py
@@ -0,0 +1,40 @@
+import torch
+
+import genesis as gs
+from genesis.sensors.imu import IMUOptions
+
+from .utils import assert_allclose
+
+
+def test_imu_sensor(show_viewer):
+    """Test if the IMU sensor returns the correct data."""
+    GRAVITY = -10.0
+
+    scene = gs.Scene(
+        sim_options=gs.options.SimOptions(
+            dt=1e-2,
+            substeps=1,
+            gravity=(0.0, 0.0, GRAVITY),
+        ),
+        show_viewer=show_viewer,
+    )
+
+    scene.add_entity(gs.morphs.Plane())
+
+    box = scene.add_entity(
+        morph=gs.morphs.Box(
+            size=(0.1, 0.1, 0.1),
+            pos=(0.0, 0.0, 0.1),
+        ),
+    )
+
+    imu = scene.add_sensor(IMUOptions(link_idx=box.base_link_idx))
+
+    scene.build()
+
+    for _ in range(100):
+        scene.step()
+
+    acc, ang = imu.read()
+    assert_allclose(acc, torch.tensor([0.0, 0.0, -GRAVITY]), tol=1e-7)
+    assert_allclose(ang, torch.tensor([0.0, 0.0, 0.0]), tol=1e-7)
